0d0a4b8a1c34013ce3c9151d07099ff0a5183898
micchonofficial <134895800+micchonofficial@users.noreply.github.com>
2023-05-28 21:57:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+erp=0
+
+//Se quiser alterar para a sua foto de usuário do github é só colocar no lugar do "RickHub2002" o seu nome de usuário.
+
+const getUrl = () => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        profileImg.src = data.avatar_url
+    });
+};
+
+getUrl();
